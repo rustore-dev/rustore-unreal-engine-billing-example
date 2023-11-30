40ca0b893eab220e08f9bf31f86e89826ccd6b48
rpelmegov <rpelmegov@ya.ru>
2023-11-30 11:37:16 +0300

    Add theme switcher

---
+++ Plugins/RuStoreBilling/Source/RuStoreBilling/Public/EURuStoreTheme.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EURuStoreTheme.generated.h"
+
+UENUM(BlueprintType)
+enum class EURuStoreTheme : uint8
+{
+    DARK UMETA(DisplayName = "DARK"),
+    LIGHT UMETA(DisplayName = "LIGHT")
+};
